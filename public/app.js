325791be7272650ea5efc6f41df9199f6dfb62ea
dwij-25 <dwijpancholi@gmail.com>
2025-05-09 21:29:08 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-const socket = io('ws://localhost:3500')
+const socket = io() // Automatically connects to same origin in production
+
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
